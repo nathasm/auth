67584d7da4bb98f46d1de7d8062846cab2429ea6
Flagship 2 <flagship2@charlestoncodes.com>
2012-06-27 19:48:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '480541fd00bcd6ffdcf45a67943dda4858361a6ff8dbfd34be9583d5a8503cfdd179d33d91eefb5821a171acf66b3b15724d7c4f55118ccd7861eaa3308855c7'
